fe36bfc59334e8d959e613946d68db15cebd18c6
sarwin_ <64829298+SarwinR@users.noreply.github.com>
2022-11-02 20:38:49 +0000

    Project Completed

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer')
+const os = require("os");
+const tempDir = os.tmpdir();
+
 require('dotenv').config()
 
 var app = express();
@@ -11,8 +15,10 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+const upload = multer({ dest: tempDir + '/uploads' })
+app.post('/api/fileanalyse',upload.single('upfile'), function(req, res){
+    res.json({"name": req.file.originalname, "type":req.file.mimetype, "size": req.file.size})
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
